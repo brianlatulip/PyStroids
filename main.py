7901d3b594ebfcb4b160a945c5e743347dd36aaa
Brian LaTulip <blatul2@wgu.edu>
2025-02-05 00:10:32 -0600

    added groups to handle game objects, cleaned up code

--- main.py
+++ main.py
@@ -5,25 +5,29 @@ from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    game_over = False
     clock = pygame.time.Clock()
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    game_over = False
     dt = 0
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
-    
+
     while game_over != True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        player.update(dt)
+        updateable.update(dt) #updates items in updateable group
+
         screen.fill("black")
-        player.draw(screen)
+
+        for drw in drawable: #draws items in drawable group to screen
+            drw.draw(screen)
+
         pygame.display.flip()
         
-        #limit framerate to 60 fps
-        dt = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000 #limit framerate to 60 fps
     
 if __name__ == "__main__":
     main()
