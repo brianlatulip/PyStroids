fba3bc176df4417c8e718faa7a4a174f6649fe6b
blatulip <blatulip@outlook.com>
2025-02-04 20:13:26 -0600

    added clock object and time tracking

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_over = False
     
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while game_over != True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        
+        dt = clock.tick(60) / 1000
     
 if __name__ == "__main__":
     main()
