d9a109a551c371f0c9a49e9f41fb533a16de2803
blatulip <blatulip@outlook.com>
2025-02-04 21:56:11 -0600

    added ability to rotate player sprite left/right on a/d press

--- main.py
+++ main.py
@@ -16,11 +16,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
-        
-        
         pygame.display.flip()
+        
+        #limit framerate to 60 fps
         dt = clock.tick(60) / 1000
     
 if __name__ == "__main__":
