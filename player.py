e3a904eb0253f69800bf885f417e09bbd3398f8c
blatulip <blatulip@outlook.com>
2025-02-05 11:55:09 -0600

    added shot class, ability to shoot bullets

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from shot import Shot
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, SHOT_RADIUS, PLAYER_SHOT_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -32,8 +33,15 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot(dt)
     
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+        
+    def shoot(self, dt):
+        bullet = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+        bullet.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOT_SPEED
+        
         
---
+++ shot.py
@@ -0,0 +1,17 @@
+import pygame
+from constants import *
+from circleshape import CircleShape
+
+
+class Shot(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.rotation = 0
+        
+    def draw(self, screen):
+        pygame.draw.circle(screen, "red", self.position, SHOT_RADIUS)
+        
+    def update(self, dt):
+        self.position += (self.velocity * dt)
+    
+    
